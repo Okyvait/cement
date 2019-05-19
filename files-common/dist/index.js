2dcae34af76f3f40dc934b09552312783ffbc19f
Смирнова Татьяна Анатольевна <khlopenuk.tatyana@skbkontur.ru>
2019-05-19 20:17:52 +0500

    draw a simple graph with vis js

---
+++ files-common/dist/index.js
@@ -0,0 +1,31 @@
+console.log(deps)
+// create an array with nodes
+const nodes = new vis.DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+  { id: 5, label: 'Node 5' }
+]);
+
+// create an array with edges
+const edges = new vis.DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 }
+]);
+
+// create a network
+const container = document.getElementById('graph');
+
+// provide the data in the vis format
+const data = {
+  nodes: nodes,
+  edges: edges
+};
+const options = {};
+
+// initialize your network!
+const network = new vis.Network(container, data, options);
+// network.enableEditMode();
